647c49a858cd5cc4280231735b409e688178fcfd
Nyi Nyi Htun Lwin <nnhl.mgk@gmail.com>
2024-03-23 20:34:07 +0630

    integrated: meal categories api

--- feature/meals/src/main/java/projects/nyinyihtunlwin/meals/presentation/screens/category/MealCategoryListScreen.kt
+++ feature/meals/src/main/java/projects/nyinyihtunlwin/meals/presentation/screens/category/MealCategoryListScreen.kt
@@ -2,24 +2,33 @@
 
 package projects.nyinyihtunlwin.meals.presentation.screens.category
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -28,10 +37,12 @@ import eu.bambooapps.material3.pullrefresh.PullRefreshIndicator
 import eu.bambooapps.material3.pullrefresh.PullRefreshIndicatorDefaults
 import eu.bambooapps.material3.pullrefresh.pullRefresh
 import eu.bambooapps.material3.pullrefresh.rememberPullRefreshState
+import projects.nyinyihtunlwin.designsystem.components.DiyRecipesCard
 import projects.nyinyihtunlwin.designsystem.components.DiyRecipesToolbar
 import projects.nyinyihtunlwin.designsystem.theme.DiyRecipesTheme
 import projects.nyinyihtunlwin.designsystem.theme.LocalSpacing
 import projects.nyinyihtunlwin.designsystem.theme.Mix_White
+import projects.nyinyihtunlwin.designsystem.theme.Mix_White_50
 import projects.nyinyihtunlwin.designsystem.theme.Orange
 import projects.nyinyihtunlwin.meals.presentation.model.MealCategoryUiModel
 
@@ -49,7 +60,6 @@ fun MealCategoryListScreen(
             viewModel.getMealCategories()
         },
         onCategorySelected = {
-            TODO("Navigate to Details")
         })
 }
 
@@ -80,7 +90,15 @@ fun MealCategoryListContent(
             LazyVerticalGrid(
                 modifier = Modifier
                     .fillMaxWidth(),
-                columns = GridCells.Fixed(2)
+                columns = GridCells.Fixed(2),
+                horizontalArrangement = Arrangement.spacedBy(LocalSpacing.current.spaceXSmall),
+                verticalArrangement = Arrangement.spacedBy(LocalSpacing.current.spaceXSmall),
+                contentPadding = PaddingValues(
+                    bottom = 100.dp,
+                    top = LocalSpacing.current.spaceNormal,
+                    start = LocalSpacing.current.spaceNormal,
+                    end = LocalSpacing.current.spaceNormal
+                )
             ) {
                 items(items = categories) { data ->
                     MealCategoryItem(data = data, onCategorySelected = onCategorySelected)
@@ -105,12 +123,38 @@ fun MealCategoryItem(
     data: MealCategoryUiModel,
     onCategorySelected: (MealCategoryUiModel) -> Unit
 ) {
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(150.dp)
+    DiyRecipesCard(
+        onClick = {
+            onCategorySelected.invoke(data)
+        }
     ) {
-        AsyncImage(model = data.strCategoryThumb, contentDescription = null, contentScale = ContentScale.Crop)
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(250.dp)
+        ) {
+            AsyncImage(
+                modifier = Modifier.fillMaxSize(),
+                model = data.strCategoryThumb,
+                contentDescription = null,
+                contentScale = ContentScale.Fit
+            )
+            Text(
+                text = data.strCategory,
+                style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.Normal),
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(end = LocalSpacing.current.spaceSmall, bottom = LocalSpacing.current.spaceSmall)
+                    .clip(shape = RoundedCornerShape(topEnd = 12.dp, bottomEnd = 12.dp))
+                    .background(Mix_White_50)
+                    .padding(LocalSpacing.current.spaceSmall)
+                    .align(Alignment.BottomStart),
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 3,
+                color = Orange,
+            )
+        }
     }
 }
 
